7df77c750636761e66b44599cde50ad534c8196a
pierrel2025-rgb <265147442+pierrel2025-rgb@users.noreply.github.com>
2026-06-02 12:12:33 +0200

    Added user authentication

--- app.py
+++ app.py
@@ -1,10 +1,100 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
+from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
 
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+Session(app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    if request.method == "POST":
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        hashed = generate_password_hash(password)
+
+        conn = sqlite3.connect("checkpoint.db")
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute(
+                "INSERT INTO users (username, hash) VALUES (?, ?)",
+                (username, hashed)
+            )
+
+            conn.commit()
+
+        except:
+
+            conn.close()
+
+            return "Username already exists"
+
+        conn.close()
+
+        return redirect("/login")
+
+    return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    session.clear()
+
+    if request.method == "POST":
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        conn = sqlite3.connect("checkpoint.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "SELECT * FROM users WHERE username = ?",
+            (username,)
+        )
+
+        user = cursor.fetchone()
+
+        conn.close()
+
+        if user is None:
+            return "Invalid username"
+
+        if not check_password_hash(user[2], password):
+            return "Invalid password"
+
+        session["user_id"] = user[0]
+
+        return redirect("/dashboard")
+
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
+
+    if "user_id" not in session:
+        return redirect("/login")
+
+    return render_template("dashboard.html")
+
+@app.route("/logout")
+def logout():
+
+    session.clear()
+
+    return redirect("/login")
 
 if __name__ == "__main__":
     app.run(debug=True)
